5d35ea338a41c742c4daf7f951ae8b1d6c3de781
Namima Islam <namimaislam308@gmail.com>
2024-09-24 11:18:14 -0400

    lab

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-//this function should include a fetch request to the Game of Thrones API (https://anapioficeandfire.com/api/books to an external site.). The returned response should be converted to JSON. Then, it should call the second function, renderBooks(), passing in the JSON-ified data as the argument. To check if you have done this correctly, open up the index.html page of this lab; you should see a list of Game Of Thrones titles on your webpage.
+//To pass this lab, this function should include a fetch request to the Game of Thrones API (https://anapioficeandfire.com/api/books Links to an external site.). 
+//The returned response should be converted to JSON. 
+//Then, it should call the second function, renderBooks(), passing in the JSON-ified data as the argument. To check if you have done this correctly, open up the index.html page of this lab; you should see a list of Game Of Thrones titles on your webpage.
 
-const apiUrl = "https://anapioficeandfire.com/api/books"
+//fetchBooks has empty para bc there's no addition tasks
+function fetchBooks(){
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data));
+};
 
-function fetchBooks(url){
-  return fetch("https://anapioficeandfire.com/api/books")
-  .then(response => response.json()) 
-  .then(data => renderBooks(data))
-}
-  
-
+//second then is sending the data to a function that will append to the DOM
+ 
+//querySelector - helps us target something from the dom, by id, class or type
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book => {  //the for each is going through each element
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -19,5 +22,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks(apiUrl);
+  fetchBooks();
 });
+
